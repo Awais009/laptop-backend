730d3ddc49d7f7922c9fb7bbb1144c25a5d70a00
Awais009 <41166466+Awais009@users.noreply.github.com>
2024-11-21 08:14:38 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Livewire\Order\OrderDetail;
 use App\Livewire\Order\OrderList;
 use App\Livewire\Products\AddProduct;
 use App\Livewire\Products\ProductList;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/admin', Dashboard::class)->name('home');
@@ -18,4 +19,7 @@ Route::get('/order-detail/{id}', OrderDetail::class)->name('order.detail');
 Route::get('/navigation', Navigations::class)->name('navigation');
 Route::get('/filter', Categories::class)->name('category');
 Route::get('/invoice/{id}', Invoice::class)->name('invoice');
+Route::get('/link', function (){
+   artisan::call('storage:link');
+});
 
