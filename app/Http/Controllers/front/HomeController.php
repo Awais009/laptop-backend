1358ea8562d92fda55b5fabc8e8111fb98e6e8a7
Awais009 <41166466+Awais009@users.noreply.github.com>
2024-11-21 17:06:44 -0800

    Update HomeController.php

--- app/Http/Controllers/front/HomeController.php
+++ app/Http/Controllers/front/HomeController.php
@@ -17,12 +17,22 @@ class HomeController extends Controller
     public function index(){
 
         $navigations = Navigation::with('products.image','products.navigation_item')->get();
-
+        $banners = Product::with('image','images')
+            ->where('type','banner')
+            ->orderByDesc('id')
+            ->get();
+        $best_products = Product::with('image', 'images')
+            ->whereNotIn('type', ['banner', 'all'])
+            ->latest()
+            ->get()
+            ->groupBy('type');
         return response()->json([
             'success' => true,
             'storagePath' => asset('storage/app'),
             'message' => 'Products retrieved successfully',
-            'navigations' => $navigations
+            'navigations' => $navigations,
+            'banners' => $banners,
+            'best_products' => $best_products
         ], 200);
     }
 }
