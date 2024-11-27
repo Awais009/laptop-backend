f5d1cae9c40cd22e0e5a72d64e295201d791b582
Awais009 <41166466+Awais009@users.noreply.github.com>
2024-11-26 18:05:13 -0800

    new

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,15 +2,80 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Navigation\Navigations;
+use App\Models\Navigation;
+use App\Models\NavigationItem;
 use App\Models\Order;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
-
+use Illuminate\Database\QueryException;
+use Illuminate\Validation\ValidationException;
 class Dashboard extends Component
 {
+
+
+
+
+
+    function formatNumber($num) {
+        $num = intval($num) ;
+
+
+        $suffixes = ['', 'K', 'M', 'B', 'T'];
+        $exp = floor(log10($num) / 3);
+        if ($num == 0) {
+            return '0';
+        }
+        $value = $num / pow(10, $exp*3);
+        $formatted = ($value == (int) $value) ? (int) $value : number_format($value, 1);
+        return $formatted . $suffixes[$exp];
+    }
     public function render()
     {
 
+        $today_revenue =  Order::where('status','delivered')-> where('created_at',now())-> sum('total');
+        $total_revenue = Order::where('status','delivered')->sum('total');
+        $monthlyAverageIncomes = [];
+        $transactions = Order::select(
+            DB::raw('COUNT(*) as count'),
+            DB::raw('MONTH(created_at) as month'),
+            DB::raw('SUM(total) as total_income')
+        )
+            ->whereYear('created_at',date('Y'))
+            ->groupBy('month')
+            ->get();
+
+
+        $totalIncome = Order::whereYear('created_at', date('Y'))->sum('total');
+        $currentMonth = date('n'); // current month (1-12)
+
+        foreach ($transactions as $transaction) {
+            $monthlyAverageIncomes[$transaction->month] = [
+                'month' => date('M', mktime(0, 0, 0, $transaction->month, 1)),
+                'total_income' => $this->formatNumber($transaction->total_income) ,
+                'count' => $transaction->count ,
+                'percentage' => ($totalIncome != 0) ? round(($transaction->total_income / $totalIncome) * 100, 2) : 0,
+                'color' => ($transaction->month == $currentMonth) ? '#22c55e' : '#95a0c5',
+            ];
+        }
+
+// Fill missing months with 0
+        for ($i = 1; $i <= 12; $i++) {
+            if (!isset($monthlyAverageIncomes[$i])) {
+                $monthlyAverageIncomes[$i] = [
+                    'month' => date('M', mktime(0, 0, 0, $i, 1)),
+                    'total_income' => 0,
+                    'percentage' => 0,
+                    'count' => 0,
+                    'color' => '#95a0c5',
+                ];
+            }
+        }
+
+// Sort array by key (month)
+        ksort($monthlyAverageIncomes);
+
+//        dd($monthlyAverageIncomes);
         $orderPercentages = Order::whereIn('status', ['pending', 'cancelled', 'delivered'])
             ->whereYear('created_at', date('Y'))
             ->select(
@@ -25,6 +90,15 @@ class Dashboard extends Component
             ->toArray();
 //        dd($orderPercentages);
 
-        return view('livewire.dashboard');
+        $navigations = Navigation::with('products')
+            ->select('id', 'title')
+            ->get()
+            ->map(function ($navigation) {
+                return[ 'count' => $navigation->products->count(),'name' => $navigation->title];
+            })
+            ->toArray();
+
+
+        return view('livewire.dashboard', compact('navigations', 'orderPercentages','monthlyAverageIncomes','total_revenue','today_revenue'));
     }
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,7 +1,21 @@
 
         <div class="container-xxl">
             <div class="row">
+@php
+    function formatNumber($num) {
+            $num = intval($num) ;
 
+
+            $suffixes = ['', 'K', 'M', 'B', 'T'];
+            $exp = floor(log10($num) / 3);
+            if ($num == 0) {
+                return '0';
+            }
+            $value = $num / pow(10, $exp*3);
+            $formatted = ($value == (int) $value) ? (int) $value : number_format($value, 1);
+            return $formatted . $suffixes[$exp];
+        }
+@endphp
                 <div class="col-md-12 col-lg-12 col-xl-12">
                     <div class="card">
                         <div class="card-header">
@@ -32,7 +46,7 @@
                                         <div class="card-body">
                                             <div class="row align-items-center">
                                                 <div class="col text-center">
-                                                    <span class="fs-18 fw-semibold">$24,500</span>
+                                                    <span class="fs-18 fw-semibold">${{number_format($today_revenue)}}</span>
                                                     <h6 class="text-uppercase text-muted mt-2 m-0">Today's Revenue</h6>
                                                 </div><!--end col-->
                                             </div> <!-- end row -->
@@ -87,7 +101,7 @@
                         <div class="card-header">
                             <div class="row align-items-center">
                                 <div class="col">
-                                    <h4 class="card-title">Customers</h4>
+                                    <h4 class="card-title">Orders</h4>
                                 </div><!--end col-->
                                 <div class="col-auto">
                                     <div class="img-group d-flex">
@@ -112,10 +126,7 @@
                         </div>
                         <div class="card-body pt-0">
                             <div id="customers" class="apex-charts"></div>
-                            <div class="bg-light py-3 px-2 mb-0 mt-3 text-center rounded">
-                                <h6 class="mb-0"><i class="icofont-calendar fs-5 me-1"></i>  01 January 2024 to 31 December 2024</h6>
-                            </div>
-                            <p class="text-secondary text-truncate mb-0 rounded mt-2 text-center">To review the new order by Jems. <a href="#" class="text-primary text-decoration-underline">detail</a> </p>
+
                         </div><!--end card-body-->
                     </div><!--end card-->
                 </div> <!--end col-->
@@ -138,7 +149,7 @@
                         <div class="card-body">
                             <div class="row d-flex justify-content-center ">
                                 <div class="col-12 col-md-6">
-                                    <h2 class="fs-22 mt-0 mb-1 fw-bold">$8365.00</h2>
+                                    <h2 class="fs-22 mt-0 mb-1 fw-bold">${{number_format($total_revenue)}}</h2>
                                     <p class="mb-0 text-truncate text-muted"><span class="text-success"><i class="mdi mdi-trending-up"></i>8.5%</span> New Sessions Today</p>
                                 </div><!--end col-->
                                 <div class="col-12 col-md-6 align-self-center text-start text-md-end">
@@ -182,103 +193,122 @@
             <script>
                 document.addEventListener('livewire:initialized', () => {
 
-                var options1 = {
-                        series: [{ data: [25, 66, 41, 89, 63, 25, 44, 12, 36, 9, 54] }],
-                        chart: { type: "line", width: 120, height: 35, sparkline: { enabled: !0 }, dropShadow: { enabled: !0, top: 4, left: 0, bottom: 0, right: 0, blur: 2, color: "rgba(132, 145, 183, 0.3)", opacity: 0.35 } },
-                        colors: ["#95a0c5"],
-                        stroke: { show: !0, curve: "smooth", width: [3], lineCap: "round" },
-                        tooltip: {
-                            fixed: { enabled: !1 },
-                            x: { show: !1 },
-                            y: {
-                                title: {
-                                    formatter: function (o) {
-                                        return "";
-                                    },
-                                },
-                            },
-                            marker: { show: !1 },
-                        },
-                    },
-                    chart1 = new ApexCharts(document.querySelector("#line-1"), options1),
-                    options2 =
-                        (
-                    chart2 = new ApexCharts(document.querySelector("#line-2"), options2),
-                    colors = (chart2.render(), ["#95a0c5", "#95a0c5", "#95a0c5", "#22c55e", "#95a0c5", "#95a0c5", "#95a0c5", "#95a0c5", "#95a0c5", "#95a0c5", "#95a0c5", "#95a0c5"]),
-                    options = {
-                        chart: { height: 270, type: "bar", toolbar: { show: !1 }, dropShadow: { enabled: !0, top: 0, left: 5, bottom: 5, right: 0, blur: 5, color: "#45404a2e", opacity: 0.35 } },
-                        colors: colors,
-                        plotOptions: { bar: { borderRadius: 6, dataLabels: { position: "top" }, columnWidth: "20", distributed: !0 } },
-                        dataLabels: {
-                            enabled: !0,
-                            formatter: function (o) {
-                                return o + "%";
-                            },
-                            offsetY: -20,
-                            style: { fontSize: "12px", colors: ["#8997bd"] },
-                        },
-                        series: [{ name: "Inflation", data: [2.3, 3.1, 4, 10.1, 4, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2] }],
-                        xaxis: {
-                            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                            position: "top",
-                            axisBorder: { show: !1 },
-                            axisTicks: { show: !1 },
-                            crosshairs: { fill: { type: "gradient", gradient: { colorFrom: "#D8E3F0", colorTo: "#BED1E6", stops: [0, 100], opacityFrom: 0.4, opacityTo: 0.5 } } },
-                            tooltip: { enabled: !0 },
-                        },
-                        yaxis: {
-                            axisBorder: { show: !1 },
-                            axisTicks: { show: !1 },
-                            labels: {
-                                show: !0,
-                                formatter: function (o) {
-                                    return "$" + o + "k";
-                                },
-                            },
-                        },
-                        grid: { row: { colors: ["transparent", "transparent"], opacity: 0.2 }, strokeDashArray: 2.5 },
-                        legend: { show: !1 },
-                    },
-                    chart = new ApexCharts(document.querySelector("#monthly_income"), options),
-                    options =
-                        (chart.render(),
-                            {
-                                series: [{ name: "Items", data: [1380, 1100, 990, 880, 740, 548, 330, 200] }],
-                                chart: { type: "bar", height: 275, toolbar: { show: !1 } },
-                                plotOptions: { bar: { borderRadius: 6, horizontal: !0, distributed: !0, barHeight: "85%", isFunnel: !0, isFunnel3d: !1 } },
-                                dataLabels: {
-                                    enabled: !0,
-                                    formatter: function (o, e) {
-                                        return e.w.globals.labels[e.dataPointIndex];
-                                    },
-                                    dropShadow: { enabled: !1 },
-                                    style: { colors: ["#22c55e"], fontWeight: 400, fontSize: "13px" },
-                                },
-                                xaxis: { categories: ["Mobile", "Men Fishion", "Women Fishion", "Beauty", "Health", "Sports", "Kids", "Music"] },
-                                colors: ["rgba(34, 197, 94, 0.45)", "rgba(34, 197, 94, 0.4)", "rgba(34, 197, 94, 0.35)", "rgba(34, 197, 94, 0.3)", "rgba(34, 197, 94, 0.25)", "rgba(34, 197, 94, 0.2)", "rgba(34, 197, 94, 0.15)", "rgba(34, 197, 94, 0.1)"],
-                                legend: { show: !1 },
-                            }),
-                    options =
-                        ((chart = new ApexCharts(document.querySelector("#categories"), options)).render(),
-                            {
-                                chart: { height: 280, type: "donut" },
-                                plotOptions: { pie: { donut: { size: "80%" } } },
-                                dataLabels: { enabled: !1 },
-                                stroke: { show: !0, width: 2, colors: ["transparent"] },
-                                series: [50, 10, 40],
-                                legend: { show: !0, position: "bottom", horizontalAlign: "center", verticalAlign: "middle", floating: !1, fontSize: "13px", fontFamily: "Be Vietnam Pro, sans-serif", offsetX: 0, offsetY: 0 },
-                                labels: ["pending", "delivered", "cancelled"],
-                                colors: ["#22c55e", "#08b0e7", "#ffc728"],
-                                responsive: [{ breakpoint: 600, options: { plotOptions: { donut: { customScale: 0.2 } }, chart: { height: 240 }, legend: { show: !1 } } }],
-                                tooltip: {
-                                    y: {
+                    var options1 = {
+                            series: [{ data: {!! json_encode(array_column($monthlyAverageIncomes,'total_income') )  !!} }],
+                            chart: { type: "line", width: 120, height: 35, sparkline: { enabled: !0 }, dropShadow: { enabled: !0, top: 4, left: 0, bottom: 0, right: 0, blur: 2, color: "rgba(132, 145, 183, 0.3)", opacity: 0.35 } },
+                            colors: ["#95a0c5"],
+                            stroke: { show: !0, curve: "smooth", width: [3], lineCap: "round" },
+                            tooltip: {
+                                fixed: { enabled: !1 },
+                                x: { show: !1 },
+                                y: {
+                                    title: {
                                         formatter: function (o) {
-                                            return o + " %";
+                                            return "";
                                         },
                                     },
                                 },
-                            });
-                (chart = new ApexCharts(document.querySelector("#customers"), options)).render();
+                                marker: { show: !1 },
+                            },
+                        },
+                        chart1 = new ApexCharts(document.querySelector("#line-1"), options1),
+                        options2 =
+                            (chart1.render(),
+                                {
+                                    series: [{ data: [25, 66, 41, 89, 63, 25, 44, 12, 36, 9, 54] }],
+                                    chart: { type: "line", width: 120, height: 35, sparkline: { enabled: !0 }, dropShadow: { enabled: !0, top: 4, left: 0, bottom: 0, right: 0, blur: 2, color: "rgba(132, 145, 183, 0.3)", opacity: 0.35 } },
+                                    colors: ["#95a0c5"],
+                                    stroke: { show: !0, curve: "smooth", width: [3], lineCap: "round" },
+                                    tooltip: {
+                                        fixed: { enabled: !1 },
+                                        x: { show: !1 },
+                                        y: {
+                                            title: {
+                                                formatter: function (o) {
+                                                    return "";
+                                                },
+                                            },
+                                        },
+                                        marker: { show: !1 },
+                                    },
+                                }),
+                        chart2 = new ApexCharts(document.querySelector("#line-2"), options2),
+                        colors = (chart2.render(), {!! json_encode(array_column($monthlyAverageIncomes,'color') )  !!}),
+                        options = {
+                            chart: { height: 270, type: "bar", toolbar: { show: !1 }, dropShadow: { enabled: !0, top: 0, left: 5, bottom: 5, right: 0, blur: 5, color: "#45404a2e", opacity: 0.35 } },
+                            colors: colors,
+                            plotOptions: { bar: { borderRadius: 6, dataLabels: { position: "top" }, columnWidth: "20", distributed: !0 } },
+                            dataLabels: {
+                                enabled: !0,
+                                formatter: function (o) {
+                                    return o + "%";
+                                },
+                                offsetY: -20,
+                                style: { fontSize: "12px", colors: ["#8997bd"] },
+                            },
+                            series: [{ name: "Inflation", data: {!! json_encode(array_column($monthlyAverageIncomes,'percentage') )  !!} }],
+                            xaxis: {
+                                categories: {!! json_encode(array_column($monthlyAverageIncomes,'month')) !!},
+                                position: "top",
+                                axisBorder: { show: !1 },
+                                axisTicks: { show: !1 },
+                                crosshairs: { fill: { type: "gradient", gradient: { colorFrom: "#D8E3F0", colorTo: "#BED1E6", stops: [0, 100], opacityFrom: 0.4, opacityTo: 0.5 } } },
+                                tooltip: { enabled: !0 },
+                            },
+                            yaxis: {
+                                axisBorder: { show: !1 },
+                                axisTicks: { show: !1 },
+                                labels: {
+                                    show: !0,
+                                    formatter: function (o) {
+                                        return "$" + o + "k";
+                                    },
+                                },
+                            },
+                            grid: { row: { colors: ["transparent", "transparent"], opacity: 0.2 }, strokeDashArray: 2.5 },
+                            legend: { show: !1 },
+                        },
+                        chart = new ApexCharts(document.querySelector("#monthly_income"), options),
+                        options =
+                            (chart.render(),
+                                {
+                                    series: [{ name: "Items", data: {!! json_encode(array_column($navigations,'count')) !!} }],
+                                    chart: { type: "bar", height: 275, toolbar: { show: !1 } },
+                                    plotOptions: { bar: { borderRadius: 6, horizontal: !0, distributed: !0, barHeight: "85%", isFunnel: !0, isFunnel3d: !1 } },
+                                    dataLabels: {
+                                        enabled: !0,
+                                        formatter: function (o, e) {
+                                            return e.w.globals.labels[e.dataPointIndex];
+                                        },
+                                        dropShadow: { enabled: !1 },
+                                        style: { colors: ["#22c55e"], fontWeight: 400, fontSize: "13px" },
+                                    },
+                                    xaxis: { categories: {!! json_encode(array_column($navigations,'name')) !!} },
+                                    colors: ["rgba(34, 197, 94, 0.45)", "rgba(34, 197, 94, 0.4)", "rgba(34, 197, 94, 0.35)", "rgba(34, 197, 94, 0.3)", "rgba(34, 197, 94, 0.25)", "rgba(34, 197, 94, 0.2)", "rgba(34, 197, 94, 0.15)", "rgba(34, 197, 94, 0.1)"],
+                                    legend: { show: !1 },
+                                }),
+                        options =
+                            ((chart = new ApexCharts(document.querySelector("#categories"), options)).render(),
+                                {
+                                    chart: { height: 280, type: "donut" },
+                                    plotOptions: { pie: { donut: { size: "80%" } } },
+                                    dataLabels: { enabled: !1 },
+                                    stroke: { show: !0, width: 2, colors: ["transparent"] },
+                                    series: [{!! json_decode($orderPercentages['pending'] ?? 0) !!},{!! json_decode($orderPercentages['delivered'] ?? 0) !!},{!! json_decode($orderPercentages['cancelled'] ?? 0) !!}],
+                                    legend: { show: !0, position: "bottom", horizontalAlign: "center", verticalAlign: "middle", floating: !1, fontSize: "13px", fontFamily: "Be Vietnam Pro, sans-serif", offsetX: 0, offsetY: 0 },
+                                    labels: ["Pending", "Delivered", "Cancelled"],
+                                    colors: ["#08b0e7", "#22c55e", "#eb1111"],
+                                    responsive: [{ breakpoint: 600, options: { plotOptions: { donut: { customScale: 0.2 } }, chart: { height: 240 }, legend: { show: !1 } } }],
+                                    tooltip: {
+                                        y: {
+                                            formatter: function (o) {
+                                                return o + " %";
+                                            },
+                                        },
+                                    },
+                                });
+                    (chart = new ApexCharts(document.querySelector("#customers"), options)).render();
+
                 })
 
             </script>
